3044c4eb27f664016bf952dfba7066471a9002c6
Filip Wiśniewski <fwfw541111@gmail.com>
2023-03-29 23:12:01 +0200

    Working book creation

--- src/components/CreateBookComponent.vue
+++ src/components/CreateBookComponent.vue
@@ -1,23 +1,46 @@
 <template>
-  <form>
+  <form @submit="e => createBook(e)">
     <div class="inputWrapper">
       <label for="title">Title:</label>
-      <input type="text" name="title" id="title" />
+      <input type="text" name="title" id="title" v-model="book.title" />
     </div>
     <div class="inputWrapper">
       <label for="pages">Pages:</label>
-      <input type="text" name="pages" id="pages" />
+      <input type="text" name="pages" id="pages" v-model="book.pages" />
     </div>
     <div class="inputWrapper">
       <label for="authors">Authors:</label>
       <input type="text" name="authors" id="authors" />
     </div>
+    <input type="submit" value="Submit"/>
   </form>
 </template>
 
 <script>
 export default {
-  name: "CreateBookComponent"
+  name: "CreateBookComponent",
+  data() {
+    return { book: {} }
+  },
+
+  methods: {
+    createBook: function (e) {
+      e.preventDefault()
+      fetch(`http://localhost:9000/book`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({...this.book, authorIds: []})
+      } )
+          .then(r => {
+            if (r.status === 201) {
+              this.$router.push('/')
+            }
+          })
+          .catch(e => console.warn(e))
+    }
+  }
 }
 </script>
 
